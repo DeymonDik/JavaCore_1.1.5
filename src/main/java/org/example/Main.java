e42f43da66f31b9d23621d4a25d090efa1917873
DeymonDik <deymon91@gmail.com>
2022-09-14 09:49:45 +0300

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.print(a + b);
+        int[] min = new int[a];
+        for(int i = 0; i<a; i++){
+            int j = in.nextInt();
+            min[i] = j;
+        }
+        Arrays.sort(min);
+        System.out.println(min[1]);
     }
 }
