b7cb30c10aef4b331963c1e60fcf09a1b7d89959
DeymonDik <deymon91@gmail.com>
2022-09-14 09:56:51 +0300

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,13 @@
 package org.example;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int[][] matrix = new int[a][b];
-        int[] stb = new int[b];
-        int[] sta = new int[a];
-        for(int i = 0;i<a;i++)
-            for(int j = 0;j<b;j++){
-                matrix[i][j] = in.nextInt();
-                sta[i] = sta[i] + matrix[i][j];
-                stb[j] = stb[j] + matrix[i][j];
-            }
-        int rA = 0;
-        int rB = 0;
-        int k = 0;
-        for(int i = 0; i<a ; i++)
-            for(int j = 0; j<b; j++){
-                if(sta[i] == stb[j]) {
-                    k++;
-                    if(k > 1){
-                        System.out.println("ambiguous");
-                        return;
-                    }
-                    rA = i + 1;
-                    rB = j + 1;
-                }
-            }
-        if(k == 0){
-            System.out.println("none");
-            return;
-        }
-        System.out.print(rA);
-        System.out.print(" ");
-        System.out.print(rB);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+        System.out.println(a.subtract(b));
     }
 }
