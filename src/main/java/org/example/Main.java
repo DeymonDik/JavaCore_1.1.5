8f055358d7418c2900a77ff936e092a46969a0fe
DeymonDik <deymon91@gmail.com>
2022-09-14 09:43:48 +0300

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,20 @@
 package org.example;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        String[] a = str.split(" ");
-        a = Arrays.stream(a).sorted().toArray(String[]::new);
-
-        List<String> list = new ArrayList<>();
-        Collections.addAll(list, a);
-        list.sort(Comparator.comparingInt(String::length));
-        a = list.toArray(new String[0]);
-
-        String sim = "";
-        for(String el:a) {
-            System.out.print(sim + el);
-            sim = " ";
-        }
+        Scanner in =new Scanner(System.in);
+        String a1 = in.nextLine();
+        String a = a1.substring(0,a1.indexOf(" "));
+        String b = a1.substring(a1.indexOf(" ")+1);
+        int k = 0;
+        int index = -1;
+        do {
+            index = a.indexOf(b, index+1);
+            if(index>-1){System.out.println(index+1);
+                k++;}
+        }while(index > -1);
+        if(k==0)System.out.println(-1);
     }
 }
