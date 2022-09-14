112aef924b440d10908753d199e28f5b125d5199
DeymonDik <deymon91@gmail.com>
2022-09-14 09:48:20 +0300

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,11 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
-        String a1 = in.nextLine();
-        String a = a1.substring(0,a1.indexOf(" "));
-        String b = a1.substring(a1.indexOf(" ")+1);
-        int k = 0;
-        int index = -1;
-        do {
-            index = a.indexOf(b, index+1);
-            if(index>-1){System.out.println(index+1);
-                k++;}
-        }while(index > -1);
-        if(k==0)System.out.println(-1);
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.print(a + b);
     }
 }
